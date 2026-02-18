a1c94682fc1d0cfe1a5d6f729d07172b232e2735
Fiona Gertrude <fionagertrude.k@gmail.com>
2026-02-18 12:04:23 +0300

    new commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="text-center bg-gray-50 min-h-screen p-10">
+      <h1 className="text-5xl font-extrabold text-orange-500 underline">
+        Recipe Platform is Live! 🍲
+      </h1>
+      <p className="mt-4 text-gray-600">
+        
+      </p>
+      <button className="mt-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Test Tailwind Button
+      </button>
+    </div>
+  );
+}
+
+export default App;
